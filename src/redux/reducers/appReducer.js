979ba0839ffe4e44f5cb94a10b639d2651da259f
dqkhang96 <dqkhang96@gmail.com>
2019-07-11 20:35:44 +0700

    Update appReducer.js

--- src/redux/reducers/appReducer.js
+++ src/redux/reducers/appReducer.js
@@ -1,6 +1,8 @@
 import {SWITCH_LANGUAGE} from '../actions/type'
+import CustomI18n from '../../util/i18n';
 
-export default function appReducer(state={lang:"vi-VN"},action){
+
+export default function appReducer(state={lang:CustomI18n.locale},action){
     switch(action.type){
         case SWITCH_LANGUAGE:return {...state,lang:action.lang}
         default:return state
