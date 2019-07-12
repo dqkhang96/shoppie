1c6e113eaa2722cc92732e6042f84ad76e838442
Trương Anh Quốc <quocta@talent.ominext.com>
2019-07-12 09:39:46 +0700

    Update activity indicator of setting screen

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Text, ActivityIndicator } from 'react-native'
 import { changeLanguage } from '../../util/i18n'
 import { connect } from 'react-redux'
 import { switchLanguage } from '../../redux/actions'
 import AsyncStorage from '@react-native-community/async-storage';
+import Color from '../../theme/colors';
 
 class SettingsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      isLoadingVI: false,
+      isLoadingEN: false,
+    }
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={async() => {
+        {this.state.isLoadingVI
+        ? <ActivityIndicator size='large' color={Color.primary}/>
+        : <TouchableOpacity style={styles.button} onPress={async() => {
+          this.setState({isLoadingVI: true})
           changeLanguage("vi-VN"); 
           await AsyncStorage.setItem("lang","vi-VN")
           this.props.switchLanguage("vi-VN")
         }}>
           <Text>Change to Vietnamese</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={async() => {
+        </TouchableOpacity>}
+        {this.state.isLoadingEN
+        ? <ActivityIndicator size='large' color={Color.primary} />
+        : <TouchableOpacity style={styles.button} onPress={async() => {
+          this.setState({isLoadingEN: true})
           changeLanguage("en-GB"); 
           await AsyncStorage.setItem("lang","en-GB")
           this.props.switchLanguage("en-GB")
         }}>
           <Text>Change to English</Text>
-        </TouchableOpacity>
+        </TouchableOpacity>}
       </View>
     )
   }
